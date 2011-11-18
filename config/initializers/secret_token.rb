cf7e3816256add878ed95cd44db1a1e9adf29abc
gugavieira <gugavieira@gugavieira.com>
2011-11-18 17:28:39 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tripping::Application.config.secret_token = '0ea58e23f96dfd115befbc36c1243b7168564734531194d2b0fae5d3626c1bb772422d9ce612c782570a76c72c0da267175cca79b039a112184062022538b14e'
