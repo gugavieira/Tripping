bde28ba95e8cc9b309a01e55fc244f57b41f5876
gugavieira <gugavieira@gugavieira.com>
2011-11-23 22:05:20 +0100

    roll back wrong model name

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,18 +4,5 @@ class AdminController < ApplicationController
     @title = "Admin"
     @photosets = Photoset.all
     @new_photoset = Photoset.new
-    @not_imported_sets = not_imported_sets
-  end
-
-  
-  private
-
-  def not_imported_sets
-  	flickr_sets.reject{|element| Photoset.exists?(:flickr_set_id => element['id'])}
-  end
-
-  def flickr_sets
-  	flickraw_response = flickr.photosets.getList(:user_id => '26368973@N00')
-	  flickraw_response.to_a()
   end
 end
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,13 @@
 module AdminHelper
+
+  def new_photosets
+  	flickr_sets.reject{|element| Photoset.exists?(:flickr_set_id => element['id'])}
+  end
+
+  private
+
+  def flickr_sets
+  	flickraw_response = flickr.photosets.getList(:user_id => '26368973@N00')
+	flickraw_response.to_a()
+  end
 end
