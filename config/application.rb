7c610798ee8cdf0379e56021c571b97f98bdb588
gugavieira <gugavieira@gugavieira.com>
2011-11-29 23:15:51 +0100

    compiled assets

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,12 @@ module Tripping
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #Extra assets
+    config.assets.precompile += ['admin.css', 'admin.js']
+
+    #For Heroku
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
