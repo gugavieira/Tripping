1852878e99c7a7032a765115c7a2abc549ecf80a
gugavieira <gugavieira@gugavieira.com>
2011-12-14 20:08:16 +0100

    modified migration

--- db/migrate/20111213232043_change_ids_to_bigint_on_photos.rb
+++ db/migrate/20111213232043_change_ids_to_bigint_on_photos.rb
@@ -1,7 +1,7 @@
 class ChangeIdsToBigintOnPhotos < ActiveRecord::Migration
   def up
 	change_table :photos do |t|
-		t.integer :photoset_id, limit => 8
+		t.integer :photoset_id, :limit => 8
 		t.integer :flickr_photo_id, :limit => 8
 	end
 	change_table :photosets do |t|
